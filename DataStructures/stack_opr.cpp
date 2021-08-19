f384a1a11ca67c514e1ce3e2dfcc74f5159d3ceb
amk-tech-1003 <73668669+amk-tech-1003@users.noreply.github.com>
2021-08-19 17:43:32 +0530

    Update stack_opr.cpp

--- DataStructures/stack_opr.cpp
+++ DataStructures/stack_opr.cpp
@@ -1,32 +1,51 @@
-#include<iostream.h>
 #include <iostream>
-#include<math.h>
-
-int digits(int n){
-    int d=0;
-    while(n!=0){
-        d++;
-        n/=10;
-    }
-    return d;
-}
-void armStrong(int n){
-    int l = digits(n);
-    int temp;
-    int num=n;
-    int sum=0;
-    while(n!=0){
-        temp = n%10;
-        sum+= pow(temp,l);
-        n/=10;
-    }
-    if(sum == num){
-        std::cout<<"ArmStrong";}
-    else{
-        std::cout<<"Not";}
-}
+#define n 10
+class Stack{
+    public:
+        int top=-1;
+        int arr[n];
+        void push(int x){
+            if(top == n-1){
+                std::cout<<"Stack is overflow";
+            }
+            top++;
+            arr[top]=x;
+        }
+        void pop(){
+            //if stack is empty than top is -1
+            if(top<0){
+                std::cout<<"Stack is empty";
+            }
+            top--;
+            std::cout<<"\n";
+        }
+        void display(){
+            if(top == -1){
+                std::cout<<"Stack is empty";
+            }
+            int i=top;
+            while (i> -1){
+                std::cout<<arr[i]<<" ";
+                i--;
+            }
+            std::cout<<"\n";
+        }
+        void topval(){
+            std::cout<<top;
+            std::cout<<"\n";
+        }
+};
 int main() {
-   armStrong(1634);
-   
+    // Write C++ code here
+    Stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.push(40);
+    s.topval();
+    s.display();
+    s.pop();
+    s.display();
+
     return 0;
 }
