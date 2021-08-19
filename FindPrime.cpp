b08c68974510c9a8f6261698ada73529e56d72c0
amk-tech-1003 <73668669+amk-tech-1003@users.noreply.github.com>
2021-08-19 15:51:17 +0530

    Create FindPrime.cpp

---
+++ FindPrime.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+void prime(int n){
+    int i=1,count=0;
+    while(i<=n){
+        if(n%i==0){
+            count++;
+        }
+        i++;
+    }
+    if(count==2){
+        std::cout<<"Prime";
+    }
+    else
+        std::cout<<"not";
+}
+
+int main() {
+    prime(17);
+    return 0;
+}
